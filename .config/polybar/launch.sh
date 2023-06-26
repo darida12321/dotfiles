3b6c29069728b98716b1922d8de886969f31ba8a
darida12321 <dani.mihalik@gmail.com>
2023-06-26 12:26:24 +0100

    Setup polybar size and add fonts

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminate already running bars
+killall -q polybar
+
+# Launch bar
+echo "---" >> /tmp/polybar.log
+polybar polybar 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Bars launched..."
